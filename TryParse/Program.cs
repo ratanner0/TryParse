0a2acb3eb3aee1173cfc51ca4edb315a72033af5
Richard Tanner <ratanner0@gmail.com>
2022-06-29 10:34:22 -0400

    Add files via upload

---
+++ TryParse/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace TryParse
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please type a number: ");
+            string numberAsString = Console.ReadLine();
+            int parsedValue;
+
+            bool success = int.TryParse(numberAsString, out parsedValue);
+
+            if (success)
+            {
+                Console.WriteLine("Parsing successful - the number is {0}", parsedValue);
+            }
+            else
+            {
+                Console.WriteLine("Parsing failed! the value \"{0}\" is not a valid number!",numberAsString);
+            }
+                
+
+            Console.Read();
+        }
+    }
+}
